dc88c8816b78e2d81124e8f560272b5ac5c67824
DaveMirra1234 <mirradavemirrapereirasilva@gmail.com>
2026-03-10 08:50:46 -0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,14 @@
+nm=int(input("digite um numero: "))
+resto=0
+contador=0
+resultado=0
+while (numero%10=0)
+    resto=nm%10
+    nm=nm//10
+    for i in range (1,resto+1):
+        if resto%1==0:
+            contador+=1
+    if contador+=2:
+        resultado=resultado+resto
+    contador=0
+print(resultado)
